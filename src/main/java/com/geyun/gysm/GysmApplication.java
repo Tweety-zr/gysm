ed12e6c142c23d625600c96c461bcf48806461c6
cuishiliang <imcuishiliang@outlook.com>
2020-01-12 15:23:00 +0800

    提交

---
+++ src/main/java/com/geyun/gysm/GysmApplication.java
@@ -0,0 +1,13 @@
+package com.geyun.gysm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GysmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GysmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geyun/gysm/GysmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geyun.gysm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GysmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
